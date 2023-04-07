a985136c3b5b89df28b3eea77a79c1d603a3137e
Cypher <46045772+leafCyhvss@users.noreply.github.com>
2023-04-07 16:20:07 -0700

    Parking lot design

---
+++ Coding/coding1/src/main/OOD/Vehicle.java
@@ -0,0 +1,37 @@
+package main.OOD;
+
+
+import java.util.ArrayList;
+
+abstract class Vehicle {
+
+    protected int spotsNeeded;
+    protected VehicleSize size;
+    protected String licensePlate;
+
+    protected ArrayList<ParkingSpot> parkingSpots = new ArrayList<ParkingSpot>(); // id for parking where may occupy multi
+
+    public int getSpotsNeeded() {
+        return spotsNeeded;
+    }
+
+    public VehicleSize getSize() {
+        return size;
+    }
+
+    /* Park vehicle in this spot (among others, potentially) */
+    public void parkInSpot(ParkingSpot spot) {
+        parkingSpots.add(spot);
+    }
+
+    /* Remove car from spot, and notify spot that it's gone */
+    public void clearSpots() {
+        for (int i = 0; i < parkingSpots.size(); i++) {
+            parkingSpots.get(i).removeVehicle();
+        }
+        parkingSpots.clear();
+    }
+
+    public abstract boolean canFitInSpot(ParkingSpot spot);
+    public abstract void print();
+}
