57fdcb0f3d63fa8995e2107dc14361011e99ca71
mhe6 <mhe6@depaul.edu>
2023-04-06 23:36:03 -0500

    add code

---
+++ Coding/src/hw2/User.java
@@ -0,0 +1,33 @@
+package hw2;
+
+public class Guest {
+    private String userName;
+    private String userPassword;
+    private String userRole;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
+}
+
