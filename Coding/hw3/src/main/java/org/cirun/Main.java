7d26248fbbfb3be101279e6c05c56c68a252d93d
cirun <cirunzhang@gmail.com>
2023-04-08 03:24:51 -0400

    hw3 project structure setup

---
+++ Coding/hw3/src/main/java/org/cirun/Main.java
@@ -0,0 +1,8 @@
+package org.cirun;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ Coding/hw3/src/test/java/ArrayListExerciseTest.java
@@ -0,0 +1,74 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListExerciseTest {
+    /**
+     * new ArrayList()
+     * add elements
+     * get element
+     * get Size
+     * list.addAll(anotherList)
+     */
+    List<Integer> list = new ArrayList<>();
+
+    @Before
+    public void setup() {
+        list.add(1);
+        list.add(2);
+        list.add(3);
+    }
+
+    @Test
+    public void learn_Inserting_And_Retrieving() {
+        list.add(3, 4);
+        System.out.println(list);
+    }
+
+    /**
+     * remove(int index)
+     * remove(Object o)
+     * removeRange(int fromIndex, int toIndex)
+     * removeAll(Collection<?> c)
+     * clear()
+     *
+     * Update:
+     * set(int index, E e)
+     * replaceAll(UnaryOperator<E> operator)
+     *
+     * check:
+     * contains(Object o)
+     * indexOf(Object o)
+     * lastIndexOf(Object o)
+     */
+    @Test
+    public void learn_Remove_Replacing_Updating() {
+
+    }
+
+    /**
+     * iterator()
+     * hasNext()
+     * next()
+     * remove()
+     * forEachRemaining(Consumer<? super E> action) -- from Java8
+     */
+
+    @Test
+    public void learn_Iterator() {
+
+    }
+
+    /**
+     * sort(List<T> list)
+     * Collections.sort(List<T> t)
+     * Comparator.reverseOrder()
+     */
+
+    @Test
+    public void learn_Sorting() {
+
+    }
+}
