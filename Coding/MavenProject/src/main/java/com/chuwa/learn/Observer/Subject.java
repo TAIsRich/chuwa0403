1e71e3b6da133d4eca1ee6f96643e873a498ce4c
antoinexu <bowei_xu@outlook.com>
2023-04-10 00:47:31 -0600

    submit design pattern code

---
+++ Coding/MavenProject/src/main/java/com/chuwa/learn/Observer/Subject.java
@@ -0,0 +1,10 @@
+package com.chuwa.learn.Observer;
+
+public interface Subject {
+
+    void attach(Observer observer);
+
+    void detach(Observer observer);
+
+    void notify(String message);
+}
---
+++ Coding/MavenProject/src/main/java/com/chuwa/learn/Singleton/EagerLoad2/Client.java
@@ -0,0 +1,12 @@
+package com.chuwa.learn.Singleton.EagerLoad2;
+
+import com.chuwa.learn.Singleton.EagerLoad.Singleton;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Singleton instance = Singleton.getInstance();
+        Singleton instance1 = Singleton.getInstance();
+        System.out.println(instance == instance1);
+    }
+}
