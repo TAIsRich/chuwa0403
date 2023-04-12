5d421df998d986913e03cd396231e4e9598fa393
mhe6 <mhe6@depaul.edu>
2023-04-12 16:54:34 -0500

    add redbook 01_post 02_RUD

---
+++ MavenProject/hw4/redbook/src/main/java/com/chuwa/redbook/service/impl/PostServiceImplement.java
@@ -0,0 +1,104 @@
+package com.chuwa.redbook.service.impl;
+
+import com.chuwa.redbook.dao.PostRepository;
+import com.chuwa.redbook.entity.Post;
+import com.chuwa.redbook.exception.ResourceNotFoundException;
+import com.chuwa.redbook.payload.PostDto;
+import com.chuwa.redbook.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PostServiceImplement implements PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Override
+    public PostDto createPost(PostDto postDto) {
+ /**       // 把payload转换成entity，这样才能dao去把该数据存到数据库中。
+        Post post = new Post();
+        if (postDto.getTitle() != null) {
+            post.setTitle(postDto.getTitle());
+        } else {
+            post.setTitle("");
+        }
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+        // 此时已成功把request body的信息传递给entity
+
+        // 调用Dao的save 方法，将entity的数据存储到数据库MySQL
+        // save()会返回存储在数据库中的数据
+        Post savedPost = postRepository.save(post);
+
+        // 将save() 返回的数据转换成controller/前端 需要的数据，然后return给controller
+        PostDto postResponse = new PostDto();
+        postResponse.setId(savedPost.getId());
+        postResponse.setTitle(savedPost.getTitle());
+        postResponse.setDescription(savedPost.getDescription());
+        postResponse.setContent(savedPost.getContent());
+
+        return postResponse;
+  */
+     Post post = mapToEntity(postDto);
+
+     Post savedPost = postRepository.save(post);
+
+     PostDto postResponse = mapToDto(savedPost);
+
+     return postResponse;
+    }
+
+    @Override
+    public List<PostDto> getAllPost() {
+        List<Post> posts = postRepository.findAll();
+        List<PostDto> postDtos = posts.stream().map(post -> mapToDto(post)).collect(Collectors.toList());
+        return postDtos;
+    }
+
+    @Override
+    public PostDto getPostById(long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+        return mapToDto(post);
+    }
+
+    @Override
+    public PostDto updatePost(PostDto postDto, long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+
+        Post updatePost = postRepository.save(post);
+        return mapToDto(updatePost);
+    }
+
+    @Override
+    public void deletePostById(long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+        postRepository.delete(post);
+    }
+
+    private PostDto mapToDto(Post post){
+        PostDto postDto = new PostDto();
+        postDto.setId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setDescription(post.getDescription());
+        postDto.setContent(post.getContent());
+
+        return postDto;
+
+    }
+    private Post mapToEntity(PostDto postDto){
+        Post post = new Post();
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+
+        return post;
+    }
+
+}
