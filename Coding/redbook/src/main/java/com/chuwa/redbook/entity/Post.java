744abdf85170efcf28ebff231aa7def6e680b3da
Chen Yuan <yc47084613@gmail.com>
2023-04-12 17:48:31 -0600

    hw4submit

--- Coding/redbook/src/main/java/com/chuwa/redbook/entity/Post.java
+++ Coding/redbook/src/main/java/com/chuwa/redbook/entity/Post.java
@@ -5,10 +5,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
 import jakarta.persistence.Id;
 import jakarta.persistence.UniqueConstraint;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.Table;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
