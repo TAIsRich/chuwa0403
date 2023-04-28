b46ab59c137aaaf9c664a40db7169af36b6b676c
Claudia Feng <claudiafeng@Claudias-MacBook-Pro.local>
2023-04-27 20:44:27 -0400

    hw6

--- Coding/hw6/src/OddEvenPrinter2.java
+++ Coding/hw6/src/OddEvenPrinter2.java
@@ -1,2 +1,70 @@
-package PACKAGE_NAME;public class OddEvenPrinter2 {
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class OddEvenPrinter2 {
+
+    private final ReentrantLock lock;
+    private final Condition oddCondition;
+    private final Condition evenCondition;
+    private int number;
+
+    public OddEvenPrinter2() {
+        this.lock = new ReentrantLock();
+        this.oddCondition = lock.newCondition();
+        this.evenCondition = lock.newCondition();
+        this.number = 1;
+    }
+
+    public void printOdd() {
+        lock.lock();
+        try {
+            while (number <= 9) {
+                System.out.println("Thread 1 (odd): " + number);
+                number++;
+                evenCondition.signal();
+                if (number <= 9) {
+                    oddCondition.await();
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void printEven() {
+        lock.lock();
+        try {
+            while (number <= 10) {
+                System.out.println("Thread 2 (even): " + number);
+                number++;
+                oddCondition.signal();
+                if (number <= 10) {
+                    evenCondition.await();
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        OddEvenPrinter2 printer = new OddEvenPrinter2();
+
+        Thread oddThread = new Thread(printer::printOdd);
+        Thread evenThread = new Thread(printer::printEven);
+
+        oddThread.start();
+        evenThread.start();
+
+        try {
+            oddThread.join();
+            evenThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- Coding/hw6/src/PrintNumber.java
+++ Coding/hw6/src/PrintNumber.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class PrintNumber {
+public class PrintNumber {
+    private static int n = 1;
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(PrintNumber::printNumber);
+        Thread t2 = new Thread(PrintNumber::printNumber);
+        Thread t3 = new Thread(PrintNumber::printNumber);
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+
+    private static synchronized void printNumber() {
+        int count = 10;
+        while (count-- > 0) {
+            System.out.println(Thread.currentThread().getName() + ": " + n++);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        PrintNumber.class.notifyAll();
+    }
+
 }
