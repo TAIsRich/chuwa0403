380d1bd4ed965032ce552e9f3e03e5f9d2ef19fb
Hidoikoto <jianyuc955@gmail.com>
2023-05-15 05:26:54 -0400

    hw10

--- redbook/src/main/java/com/chuwa/redbook/service/impl/PostServiceImpl.java
+++ redbook/src/main/java/com/chuwa/redbook/service/impl/PostServiceImpl.java
@@ -1,56 +1,51 @@
 package com.chuwa.redbook.service.impl;
 
-import com.chuwa.redbook.dao.PostJPQLRepository;
 import com.chuwa.redbook.dao.PostRepository;
 import com.chuwa.redbook.entity.Post;
 import com.chuwa.redbook.exception.ResourceNotFoundException;
 import com.chuwa.redbook.payload.PostDto;
+import com.chuwa.redbook.payload.PostResponse;
 import com.chuwa.redbook.service.PostService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
+/**
+ * @author b1go
+ * @date 8/22/22 6:56 PM
+ */
 @Service
 public class PostServiceImpl implements PostService {
 
     @Autowired
     private PostRepository postRepository;
 
+    /**
+     * use this modelMapper to replace the mapToDto, mapToEntity methods.
+     */
     @Autowired
-    PostJPQLRepository postJPQLRepository;
+    private ModelMapper modelMapper;
 
     @Override
     public PostDto createPost(PostDto postDto) {
-        // 把payload转换成entity，这样才能dao去把该数据存到数据库中。
-//        Post post = new Post();
-//        if (postDto.getTitle() != null) {
-//            post.setTitle(postDto.getTitle());
-//        } else {
-//            post.setTitle("");
-//        }
-//        post.setDescription(postDto.getDescription());
-//        post.setContent(postDto.getContent());
-        // 此时已成功把request body的信息传递给entity
 
         // covert DTO to Entity
-        Post post = mapToEntity(postDto);
+//        Post post = mapToEntity(postDto);
+        Post post = modelMapper.map(postDto, Post.class);
 
         // 调用Dao的save 方法，将entity的数据存储到数据库MySQL
         // save()会返回存储在数据库中的数据
         Post savedPost = postRepository.save(post);
 
         // 将save() 返回的数据转换成controller/前端 需要的数据，然后return给controller
-//        PostDto postResponse = new PostDto();
-//        postResponse.setId(savedPost.getId());
-//        postResponse.setTitle(savedPost.getTitle());
-//        postResponse.setDescription(savedPost.getDescription());
-//        postResponse.setContent(savedPost.getContent());
+//        PostDto postResponse = mapToDTO(savedPost);
 
-        PostDto postResponse = mapToDTO(savedPost);
-
-        return postResponse;
+        return modelMapper.map(savedPost, PostDto.class);
     }
 
     /**
@@ -60,7 +55,7 @@ public class PostServiceImpl implements PostService {
     @Override
     public List<PostDto> getAllPost() {
         List<Post> posts = postRepository.findAll();
-        List<PostDto> postDtos = posts.stream().map(post -> mapToDTO(post)).collect(Collectors.toList());
+        List<PostDto> postDtos = posts.stream().map(post -> modelMapper.map(post, PostDto.class)).collect(Collectors.toList());
         return postDtos;
     }
 
@@ -78,7 +73,7 @@ public class PostServiceImpl implements PostService {
 
         Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
 
-        return mapToDTO(post);
+        return modelMapper.map(post, PostDto.class);
     }
 
     @Override
@@ -90,7 +85,7 @@ public class PostServiceImpl implements PostService {
         post.setContent(postDto.getContent());
 
         Post updatePost = postRepository.save(post);
-        return mapToDTO(updatePost);
+        return modelMapper.map(updatePost, PostDto.class);
     }
 
     @Override
@@ -101,50 +96,29 @@ public class PostServiceImpl implements PostService {
     }
 
     @Override
-    public List<PostDto> getAllPostWithJPQL() {
-        return postJPQLRepository.getAllPostWithJPQL().stream().map(post -> mapToDTO(post)).collect(Collectors.toList());
-    }
+    public PostResponse getAllPost(int pageNo, int pageSize, String sortBy, String sortDir) {
 
-    @Override
-    public PostDto getPostByIdJPQLIndexParameter(Long id, String title) {
-        Post post = postRepository.getPostByIDOrTitleWithJPQLIndexParameters(id, title);
-        return mapToDTO(post);
-    }
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending()
+                : Sort.by(sortBy).descending();
 
-    @Override
-    public PostDto getPostByIdJPQLNamedParameter(Long id, String title) {
-        Post post = postRepository.getPostByIDOrTitleWithJPQLNamedParameters(id, title);
-        return mapToDTO(post);
-    }
+        // create pageable instance
 
-    @Override
-    public PostDto getPostByIdSQLIndexParameter(Long id, String title) {
-        Post post = postRepository.getPostByIDOrTitleWithSQLIndexParameters(id, title);
-        return mapToDTO(post);
-    }
+        PageRequest pageRequest = PageRequest.of(pageNo, pageSize, sort);
+//        PageRequest pageRequest = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
+//        PageRequest pageRequest = PageRequest.of(pageNo, pageSize, Sort.by(sortBy).descending());
+        Page<Post> pagePosts = postRepository.findAll(pageRequest);
 
-    @Override
-    public PostDto getPostByIdSQLNamedParameter(Long id, String title) {
-        Post post = postRepository.getPostByIDOrTitleWithSQLNamedParameters(id, title);
-        return mapToDTO(post);
-    }
+        // get content for page abject
+        List<Post> posts = pagePosts.getContent();
+        List<PostDto> postDtos = posts.stream().map(post -> modelMapper.map(post, PostDto.class)).collect(Collectors.toList());
 
-    private PostDto mapToDTO(Post post) {
-        PostDto postDto = new PostDto();
-        postDto.setId(post.getId());
-        postDto.setTitle(post.getTitle());
-        postDto.setDescription(post.getDescription());
-        postDto.setContent(post.getContent());
-
-        return postDto;
-    }
-
-    private Post mapToEntity(PostDto postDto){
-        Post post = new Post();
-        post.setTitle(postDto.getTitle());
-        post.setDescription(postDto.getDescription());
-        post.setContent(postDto.getContent());
-
-        return post;
+        PostResponse postResponse = new PostResponse();
+        postResponse.setContent(postDtos);
+        postResponse.setPageNo(pagePosts.getNumber());
+        postResponse.setPageSize(pagePosts.getSize());
+        postResponse.setTotalElements(pagePosts.getTotalElements());
+        postResponse.setTotalPages(pagePosts.getTotalPages());
+        postResponse.setLast(pagePosts.isLast());
+        return postResponse;
     }
 }
