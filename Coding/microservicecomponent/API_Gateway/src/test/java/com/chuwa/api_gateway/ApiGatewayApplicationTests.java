3792caa374861fb2621e16ea687754ef073950f3
antoinexu <bowei_xu@outlook.com>
2023-05-21 16:27:24 -0600

    submit hw12

---
+++ Coding/microservicecomponent/API_Gateway/src/test/java/com/chuwa/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuwa.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Coding/microservicecomponent/VaccinationCenter/src/main/java/com/chuwa/vaccinationcenter/entity/VaccinationCenter.java
@@ -0,0 +1,23 @@
+package com.chuwa.vaccinationcenter.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VaccinationCenter {
+
+    @Id
+    private int id;
+
+    private String centerName;
+
+    private String centerAddress;
+
+}
